b75aed06f139f4813442af0c82ea1ed7f0bc9060
MinusMarigold98 <starehsohili@gmail.com>
2023-04-24 17:49:56 +0330

    Create "Form Wave" Mini-Project

---
+++ 8. Form Wave/script.js
@@ -0,0 +1,8 @@
+let labels = document.querySelectorAll(".label")
+
+labels.forEach(function (label) {
+    label.innerHTML = label.innerText
+        .split("")
+        .map((letter, index) => `<span style="transition-delay:${index * 50}ms">${letter}</span>`)
+        .join("")
+})
